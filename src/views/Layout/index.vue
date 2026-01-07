ba1ab8f1e51662874eaebc5893e4f163f861cc4a
laixiaobo <1483628145l@gmail.com>
2026-01-07 15:10:46 +0800

    lauout静态资源引入

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <div>Layout</div>
+  <LayoutNav></LayoutNav>
+  <LayoutHeader></LayoutHeader>
   <router-view></router-view>
+  <LayoutFooter></LayoutFooter>
 </template>
 
 <script setup>
+
+import LayoutNav from './components/LayoutNav.vue';
+import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutFooter from './components/LayoutFooter.vue';
 </script>
 
 <style scoped></style>
