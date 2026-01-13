32af9c61aff3772fb16374f51a16c6b8bb6139ae
laixiaobo <1483628145l@gmail.com>
2026-01-13 14:59:04 +0800

    完成images通用组件

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -49,7 +49,7 @@ const title = '周热榜'
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <Images></Images>
+              <Images :imageList="goodDetail.mainPictures"></Images>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
